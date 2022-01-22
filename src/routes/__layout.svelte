875f6ccd892f0bdbd9ffd37f93770d418e2c5853
Wouter Buckens <wouter@epicteddy.com>
2022-01-22 16:18:19 +0100

    Fix footer not working on creature page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -19,7 +19,7 @@ onMount(async () => {
 		<slot />
 	{/if}
 
-	<footer class="sm:flex justify-center gap-8 p-6 text-xs font-light text-stone-400 text-center">
+	<footer class="relative z-40 sm:flex justify-center gap-8 p-6 text-xs font-light text-stone-400 text-center">
 		<p>Game &copy; <a href="https://krgamestudios.com" target="_blank" rel="external" class="underline hocus:text-violet-600">KR Game Studios</a></p>
 		<p>Client by <a href="https://www.woubuc.be" target="_blank" rel="external" class="underline hocus:text-violet-600">@woubuc</a></p>
 		<p>Icons by <a href="https://icons8.com" target="_blank" rel="external" class="underline hocus:text-violet-600">Icons8</a></p>
